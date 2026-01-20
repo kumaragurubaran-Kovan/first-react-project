658ff853d4b59aa95a09f4ccf603542332c01ffd
kumaragurubaran-kovan <kumaragurubaran.k@kovanlabs.com>
2026-01-20 16:28:43 +0530

    Name Change - useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import ReactDOM from 'react-dom'
-import { createRoot } from 'react-dom/client'
 import './App.css'
 
 
 
-function App() {
+export function App() {
 
-const heading = <h1 id="heading">Hello World</h1>
+const heading = <h1 id="heading">Name Changer</h1>
 return heading
 }
-export default App
+
+
+function Name(){
+  const [appName, setName] = useState("Noname");
+
+  function changeName(){
+    setName("Nowhere");
+    setTimeout(() => {
+      setName("Nowhereelse");}, 3000)
+  }
+
+  return (
+  <div>
+    <h3 id="Name">{appName}</h3>
+    <button onClick = {changeName}>
+      Change Name
+    </button>
+  </div>
+  )
+}
+
+
+export default Name
