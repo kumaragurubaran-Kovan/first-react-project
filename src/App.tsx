8b477658302720fa848ba00b5a49ec5d8e8d88f7
kumaragurubaran-kovan <kumaragurubaran.k@kovanlabs.com>
2026-01-20 11:39:39 +0530

    Counter app - UseState

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,43 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
+  const [counter, setCounter ]= useState(0);
+  
+  function Increment(){
+    if (counter<0) {
+      setCounter(0);
+    }
+    else{
+    setCounter(counter+1);
+    }
+  };
+  
+  function Decrement(){
+    if (counter<0) {
+      setCounter(0);
+    }
+    else{
+    if (counter<=0) return
+    setCounter(counter-1);
+    }
+  };
+  
+
+  return(
+    <div>
+      <button onClick={Increment}>
+        +
+      </button>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {counter}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <button onClick={Decrement}>
+        -
+      </button>
+    </div>
   )
-}
+};
+
 
 export default App
